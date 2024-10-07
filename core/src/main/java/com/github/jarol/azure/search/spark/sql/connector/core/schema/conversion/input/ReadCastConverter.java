5f22a9e09ae9d2a828b3ae47919e7d8d399d2705
carloni.luca91 <carloni.luca91@gmail.com>
2024-10-07 19:06:33 +0200

    gathered converters in a single java class

--- core/src/main/java/com/github/jarol/azure/search/spark/sql/connector/core/schema/conversion/input/ReadCastConverter.java
+++ core/src/main/java/com/github/jarol/azure/search/spark/sql/connector/core/schema/conversion/input/ReadCastConverter.java
@@ -8,45 +8,6 @@ package com.github.jarol.azure.search.spark.sql.connector.core.schema.conversion
 public final class ReadCastConverter<T>
         extends ReadTransformConverter<T> {
 
-    /**
-     * Converter for int32
-     */
-
-    public static final ReadCastConverter<Integer> INT32;
-
-    /**
-     * Converter for int64
-     */
-
-    public static final ReadCastConverter<Long> INT64;
-
-    /**
-     * Converter for doubles
-     */
-
-    public static final ReadCastConverter<Double> DOUBLE;
-
-    /**
-     * Converter for floats
-     */
-
-    public static final ReadCastConverter<Float> SINGLE;
-
-    /**
-     * Converter for booleans
-     */
-
-    public static final ReadCastConverter<Boolean> BOOLEAN;
-
-    static {
-
-        INT32 = new ReadCastConverter<>();
-        INT64 = new ReadCastConverter<>();
-        DOUBLE = new ReadCastConverter<>();
-        SINGLE = new ReadCastConverter<>();
-        BOOLEAN = new ReadCastConverter<>();
-    }
-
     @SuppressWarnings("unchecked")
     @Override
     protected T transform(Object value) {
