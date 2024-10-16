e13ac828061aa19407e0956ae6ad880709870045
carloni.luca91 <carloni.luca91@gmail.com>
2024-10-16 16:15:46 +0200

    added more integration tests to ReadSpec

--- integration/src/test/scala/com/github/jarol/azure/search/spark/sql/connector/ReadSpec.scala
+++ integration/src/test/scala/com/github/jarol/azure/search/spark/sql/connector/ReadSpec.scala
@@ -1,13 +1,56 @@
 package com.github.jarol.azure.search.spark.sql.connector
 
-import com.github.jarol.azure.search.spark.sql.connector.core.IndexDoesNotExistException
-import com.github.jarol.azure.search.spark.sql.connector.models.SimpleBean
+import com.github.jarol.azure.search.spark.sql.connector.core.{Constants, FieldFactory, IndexDoesNotExistException}
+import com.github.jarol.azure.search.spark.sql.connector.models.{AtomicBean, SimpleBean}
 import com.github.jarol.azure.search.spark.sql.connector.write.WriteConfig
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{DataTypes, StructType}
+import org.scalatest.Inspectors
 
-import java.time.LocalDate
+import java.sql.{Date, Timestamp}
+import java.time.{LocalDate, LocalDateTime}
 
 class ReadSpec
-  extends SearchSparkSpec {
+  extends SearchSparkSpec
+    with RowMixins
+      with FieldFactory
+        with Inspectors {
+
+  /**
+   * Zip together rows and numeric beans, sorting both by id
+   * @param rows rows
+   * @param beans beans
+   * @return a collection of tuples
+   */
+
+  private def zipRowsAndBeans(
+                               rows: Seq[Row],
+                               beans: Seq[AtomicBean]
+                             ): Seq[(Row, AtomicBean)] = {
+
+    rows
+      .sortBy(_.getAs[String]("id"))
+      .zip(beans.sortBy(_.id))
+  }
+
+  /**
+   * Assert that a row value has been encoded correctly w.r.t. its original value
+   * @param output output row (retrieved from a Search index)
+   * @param input a DataFrame row written to the Search index
+   * @param colName column name
+   * @param encodingFunction expected encoding function from the input to the output
+   * @tparam T encoding target type
+   */
+
+  private def assertEncoding[T](
+                                 output: Row,
+                                 input: AtomicBean,
+                                 colName: String,
+                                 encodingFunction: AtomicBean => Option[T]
+                               ): Unit = {
+
+    output.getAsOpt[T](colName) shouldBe encodingFunction(input)
+  }
 
   describe("Search dataSource") {
     describe(SHOULD) {
@@ -25,7 +68,7 @@ class ReadSpec
         }
       }
 
-      describe("read documents from a Search index") {
+      describe("read documents") {
         it("that match a filter") {
 
           val (indexName, id) = ("simple-beans", "hello")
@@ -56,7 +99,7 @@ class ReadSpec
           indexExists(indexName) shouldBe false
         }
 
-        it("selecting a set of subfields") {
+        it("selecting some fields") {
 
           val indexName = "select-beans"
           dropIndexIfExists(indexName)
@@ -81,6 +124,151 @@ class ReadSpec
 
           dropIndexIfExists(indexName)
           indexExists(indexName) shouldBe false
+        }
+
+        describe("translating") {
+
+          // Write some data
+          val indexName = "numeric-beans"
+          val notNullBean = AtomicBean("notNullId", Some(1), Some(123), Some(3.45), Some(4.56f), Some(false), Some(LocalDateTime.now()))
+          val nullBean = AtomicBean("nullId", None, None, None, None, None, None.asInstanceOf[Option[LocalDateTime]])
+          val samples = Seq(notNullBean, nullBean)
+
+          dropIndexIfExists(indexName)
+          writeToIndex(toDF(samples), indexName, "id", None)
+          Thread.sleep(5000)
+
+          describe("numeric values as") {
+            it("strings") {
+
+              // Create a schema with all string fields
+              val schemaRead = schemaOfCaseClass[AtomicBean].map {
+                sf => if (!sf.name.equals("id")) sf.copy(dataType = DataTypes.StringType) else sf
+              }
+
+              // Read data and do assertions
+              val rows: Seq[Row] = readIndex(indexName, None, None, Some(StructType(schemaRead))).collect()
+              rows should have size samples.size
+              forAll(
+                zipRowsAndBeans(rows, samples)
+              ) {
+                case (row, sample) =>
+                  assertEncoding[String](row, sample, "intValue", _.intValue.map(String.valueOf))
+                  assertEncoding[String](row, sample, "intValue", _.intValue.map(String.valueOf))
+                  assertEncoding[String](row, sample, "longValue", _.longValue.map(String.valueOf))
+                  assertEncoding[String](row, sample, "doubleValue", _.doubleValue.map(String.valueOf))
+                  assertEncoding[String](row, sample, "floatValue", _.floatValue.map(String.valueOf))
+              }
+            }
+
+            it("numeric values of different type") {
+
+              val schemaRead = createStructType(
+                createStructField("intValue", DataTypes.DoubleType),
+                createStructField("longValue", DataTypes.FloatType)
+              )
+
+              val rows = readIndex(indexName, None, None, Some(schemaRead)).collect()
+              rows should have size samples.size
+              forAll(
+                zipRowsAndBeans(rows, samples)
+              ) {
+                case (row, bean) =>
+                  assertEncoding[Double](row, bean, "intValue", _.intValue.map(_.doubleValue()))
+                  assertEncoding[Float](row, bean, "longValue", _.longValue.map(_.floatValue()))
+              }
+            }
+          }
+
+          describe("boolean values as") {
+            it("booleans") {
+
+              val schemaRead = createStructType(
+                createStructField("booleanValue", DataTypes.BooleanType)
+              )
+
+              val rows = readIndex(indexName, None, None, Some(schemaRead)).collect()
+              rows should have size samples.size
+              forAll(
+                zipRowsAndBeans(rows, samples)
+              ) {
+                case (r, s) =>
+                  assertEncoding[Boolean](r, s, "booleanBalue", _.booleanValue)
+              }
+            }
+
+            it("strings") {
+
+              val schemaRead = createStructType(
+                createStructField("booleanValue", DataTypes.StringType)
+              )
+
+              val rows = readIndex(indexName, None, None, Some(schemaRead)).collect()
+              rows should have size samples.size
+              forAll(
+                zipRowsAndBeans(rows, samples)
+              ) {
+                case (r, s) =>
+                  assertEncoding[String](r, s, "booleanBalue", _.booleanValue.map(String.valueOf))
+              }
+            }
+          }
+
+          describe("datetimeoffset values as") {
+            it("dates") {
+
+              val schemaRead = createStructType(
+                createStructField("timestampValue", DataTypes.DateType)
+              )
+
+              val rows = readIndex(indexName, None, None, Some(schemaRead)).collect()
+              rows should have size samples.size
+              forAll(
+                zipRowsAndBeans(rows, samples)
+              ) {
+                case (r, s) =>
+                  assertEncoding[Date](r, s, "timestampValue", _.timestampValue.map {
+                    t => Date.valueOf(t.toLocalDateTime.toLocalDate)
+                  })
+              }
+            }
+
+            it("timestamps") {
+
+              val schemaRead = createStructType(
+                createStructField("timestampValue", DataTypes.TimestampType)
+              )
+
+              val rows = readIndex(indexName, None, None, Some(schemaRead)).collect()
+              rows should have size samples.size
+              forAll(
+                zipRowsAndBeans(rows, samples)
+              ) {
+                case (r, s) =>
+                  assertEncoding[Timestamp](r, s, "timestampValue", _.timestampValue)
+              }
+            }
+
+            it("strings") {
+
+              val schemaRead = createStructType(
+                createStructField("timestampValue", DataTypes.StringType)
+              )
+
+              val rows = readIndex(indexName, None, None, Some(schemaRead)).collect()
+              rows should have size samples.size
+              forAll(
+                zipRowsAndBeans(rows, samples)
+              ) {
+                case (r, s) =>
+                  assertEncoding[String](r, s, "timestampValue", _.timestampValue.map {
+                    _.toLocalDateTime.format(Constants.DATETIME_OFFSET_FORMATTER)
+                  })
+              }
+            }
+          }
+
+          dropIndexIfExists(indexName)
         }
       }
     }
--- src/main/scala/com/github/jarol/azure/search/spark/sql/connector/SearchTableProvider.scala
+++ src/main/scala/com/github/jarol/azure/search/spark/sql/connector/SearchTableProvider.scala
@@ -9,7 +9,7 @@ import org.apache.spark.sql.sources.DataSourceRegister
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.util.CaseInsensitiveStringMap
 
-import java.util
+import java.util.{Map => JMap}
 
 /**
  * [[TableProvider]] implementation for Search dataSource
@@ -22,7 +22,6 @@ class SearchTableProvider
 
   /**
    * Infer the schema for a target Search index
- *
    * @param options options for retrieving the Search index
    * @throws IndexDoesNotExistException if the target index does not exist
    * @return the index schema
@@ -57,7 +56,7 @@ class SearchTableProvider
   override def getTable(
                          schema: StructType,
                          partitioning: Array[Transform],
-                         properties: util.Map[String, String]
+                         properties: JMap[String, String]
                        ): Table = {
 
     val config = new SearchIOConfig(
@@ -70,9 +69,27 @@ class SearchTableProvider
     )
   }
 
+  /**
+   * Returns the datasource short name
+   * @return datasource's short name
+   */
+
   override def shortName(): String = Constants.DATASOURCE_NAME
 
+  /**
+   * Returns true for data sources that accept a user-defined schema
+   * @return true as this datasource allows external metadata
+   */
+
   override def supportsExternalMetadata() = true
+
+  /**
+   * Returns the prefix that should be used for qualifying datasource options at session level.
+   * <br>
+   * For this datasource, each property prefixed with <b>spark.datasource.azure.search</b>
+   * will be propagated to each read/write operation
+   * @return prefix for datasource properties at session level
+   */
 
   override def keyPrefix(): String = Constants.DATASOURCE_NAME
 }
