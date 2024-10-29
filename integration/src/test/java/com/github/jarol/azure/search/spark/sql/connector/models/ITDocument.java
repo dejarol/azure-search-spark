4311da9bccf8494e2fe29d67953b79eeb8ac376f
carloni.luca91 <carloni.luca91@gmail.com>
2024-10-29 19:53:16 +0100

    added some Scaladoc, converted PropertySerializer to Java interface

--- integration/src/test/java/com/github/jarol/azure/search/spark/sql/connector/models/ITDocument.java
+++ integration/src/test/java/com/github/jarol/azure/search/spark/sql/connector/models/ITDocument.java
@@ -1,6 +1,15 @@
 package com.github.jarol.azure.search.spark.sql.connector.models;
 
+/**
+ * Mix-in interface for documents to use for integration testing
+ */
+
 public interface ITDocument {
+
+    /**
+     * Gets this document's id
+     * @return the document's id
+     */
 
     String id();
 }
--- integration/src/test/scala/com/github/jarol/azure/search/spark/sql/connector/models/PropertySerializer.scala
+++
@@ -1,6 +0,0 @@
-package com.github.jarol.azure.search.spark.sql.connector.models
-
-trait PropertySerializer[T] {
-
-  def toPropertyValue(v1: T): AnyRef
-}
