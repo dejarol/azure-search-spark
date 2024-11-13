a6717221e9907bf7aa569bd52b66afffa254adc8
carloni.luca91 <carloni.luca91@gmail.com>
2024-11-13 17:40:51 +0100

    added FacetNullValuePartitionITSpec

--- integration/src/test/scala/com/github/jarol/azure/search/spark/sql/connector/read/partitioning/SearchPartitionITSpec.scala
+++
@@ -1,8 +0,0 @@
-package com.github.jarol.azure.search.spark.sql.connector.read.partitioning
-
-import com.github.jarol.azure.search.spark.sql.connector.SearchSparkIntegrationSpec
-
-trait SearchPartitionITSpec
-  extends SearchSparkIntegrationSpec {
-
-}
