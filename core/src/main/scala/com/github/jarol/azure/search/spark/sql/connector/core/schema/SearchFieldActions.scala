e5f98c91fe20fca58e7ab34dbb8e9fb1c1a50594
carloni.luca91 <carloni.luca91@gmail.com>
2024-11-27 14:21:48 +0100

    defined actions for adding analyzers

--- core/src/main/scala/com/github/jarol/azure/search/spark/sql/connector/core/schema/SearchFieldActions.scala
+++ core/src/main/scala/com/github/jarol/azure/search/spark/sql/connector/core/schema/SearchFieldActions.scala
@@ -1,6 +1,6 @@
 package com.github.jarol.azure.search.spark.sql.connector.core.schema
 
-import com.azure.search.documents.indexes.models.SearchField
+import com.azure.search.documents.indexes.models.{LexicalAnalyzerName, SearchField}
 
 /**
  * Collection of methods for creating [[SearchFieldAction]](s)
@@ -33,7 +33,43 @@ object SearchFieldActions {
   }
 
   /**
-   * Get an instance of [[SearchFieldAction]] for enabling a feature
+   * Action for setting an analyzer
+   * @param analyzer analyzer to set
+   */
+
+  private case class SetAnalyzer(private val analyzer: LexicalAnalyzerName)
+    extends SearchFieldAction {
+    override def apply(field: SearchField): SearchField = {
+      field.setAnalyzerName(analyzer)
+    }
+  }
+
+  /**
+   * Action for setting a Search analyzer
+   * @param analyzer Search analyzer to add
+   */
+
+  private case class SetSearchAnalyzer(private val analyzer: LexicalAnalyzerName)
+    extends SearchFieldAction {
+    override def apply(field: SearchField): SearchField = {
+      field.setSearchAnalyzerName(analyzer)
+    }
+  }
+
+  /**
+   * Action for setting an index analyzer
+   * @param analyzer index analyzer to add
+   */
+
+  private case class SetIndexAnalyzer(private val analyzer: LexicalAnalyzerName)
+    extends SearchFieldAction {
+    override def apply(field: SearchField): SearchField = {
+      field.setIndexAnalyzerName(analyzer)
+    }
+  }
+
+  /**
+   * Get an action for enabling a feature
    * @param feature feature to enable
    * @return an action for enabling a feature
    */
@@ -41,10 +77,34 @@ object SearchFieldActions {
   final def forEnablingFeature(feature: SearchFieldFeature): SearchFieldAction = EnableFeature(feature)
 
   /**
-   * Get an instance of [[SearchFieldAction]] for disabling a feature
+   * Get an action for disabling a feature
    * @param feature feature to disable
    * @return an action for disabling a feature
    */
 
   final def forDisablingFeature(feature: SearchFieldFeature): SearchFieldAction = DisableFeature(feature)
+
+  /**
+   * Get an action for setting an analyzer
+   * @param analyzerName analyzer to set
+   * @return an action for setting an analyzer
+   */
+
+  final def forSettingAnalyzer(analyzerName: LexicalAnalyzerName): SearchFieldAction = SetAnalyzer(analyzerName)
+
+  /**
+   * Get an action for setting a Search analyzer
+   * @param analyzerName Search analyzer to set
+   * @return an action for setting a Search analyzer
+   */
+
+  final def forSettingSearchAnalyzer(analyzerName: LexicalAnalyzerName): SearchFieldAction = SetSearchAnalyzer(analyzerName)
+
+  /**
+   * Get an action for setting an index analyzer
+   * @param analyzerName index analyzer to set
+   * @return an action for setting an index analyzer
+   */
+
+  final def forSettingIndexAnalyzer(analyzerName: LexicalAnalyzerName): SearchFieldAction = SetIndexAnalyzer(analyzerName)
 }
--- core/src/test/scala/com/github/jarol/azure/search/spark/sql/connector/core/schema/SearchFieldActionsSpec.scala
+++ core/src/test/scala/com/github/jarol/azure/search/spark/sql/connector/core/schema/SearchFieldActionsSpec.scala
@@ -1,6 +1,6 @@
 package com.github.jarol.azure.search.spark.sql.connector.core.schema
 
-import com.azure.search.documents.indexes.models.SearchFieldDataType
+import com.azure.search.documents.indexes.models.{LexicalAnalyzerName, SearchField, SearchFieldDataType}
 import com.github.jarol.azure.search.spark.sql.connector.core.{BasicSpec, FieldFactory}
 
 class SearchFieldActionsSpec
@@ -9,10 +9,30 @@ class SearchFieldActionsSpec
 
   private lazy val sampleField = createSearchField("hello", SearchFieldDataType.STRING)
   private lazy val feature = SearchFieldFeature.SEARCHABLE
+  private lazy val analyzer = LexicalAnalyzerName.AR_LUCENE
+
+  /**
+   * Assert that an action added an analyzer to a Search field definition
+   * @param analyzerName analyzer to add
+   * @param actionSupplier function for generating the action responsible for adding the analyzer
+   * @param analyzerGetter function for retrieving the analyzer from a Search field
+   */
+
+  private def assertAddedAnalyzer(
+                                   analyzerName: LexicalAnalyzerName,
+                                   actionSupplier: LexicalAnalyzerName => SearchFieldAction,
+                                   analyzerGetter: SearchField => LexicalAnalyzerName
+                                 ): Unit = {
+
+    // Original field should have no analyzer, while the transformed (actual) field should have it
+    analyzerGetter(sampleField) shouldBe null
+    val actual = actionSupplier(analyzerName).apply(sampleField)
+    analyzerGetter(actual) shouldBe analyzerName
+  }
 
   describe(`object`[SearchFieldActions.type ]) {
     describe(SHOULD) {
-      describe("provide methods for creating actions that") {
+      describe("define actions that") {
         it("enable a feature") {
 
           sampleField should not be enabledFor(feature)
@@ -29,6 +49,13 @@ class SearchFieldActionsSpec
           // Now, let the action disable it
           val actual = SearchFieldActions.forDisablingFeature(feature).apply(enabledField)
           actual should not be enabledFor(feature)
+        }
+
+        it("add analyzers") {
+
+          assertAddedAnalyzer(analyzer, SearchFieldActions.forSettingAnalyzer, _.getAnalyzerName)
+          assertAddedAnalyzer(analyzer, SearchFieldActions.forSettingSearchAnalyzer, _.getSearchAnalyzerName)
+          assertAddedAnalyzer(analyzer, SearchFieldActions.forSettingIndexAnalyzer, _.getIndexAnalyzerName)
         }
       }
     }
