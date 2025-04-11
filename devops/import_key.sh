1ae29aa7505959288748d871df86923df64e7ef1
carloni.luca91 <carloni.luca91@gmail.com>
2025-04-11 17:18:25 +0200

    setting gpg to quiet

--- devops/import_key.sh
+++ devops/import_key.sh
@@ -5,4 +5,4 @@ function log() {
     echo "$(date +"%Y-%m-%d %H:%m:%S")" "[$1]" "$2"
 }
 
-echo "$1" | gpg --batch --yes --import && log "INFO" "Successfully imported key"
+echo "$1" | gpg --batch --yes --quiet --import && log "INFO" "Successfully imported key"
