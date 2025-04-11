1c0b5b45c5dd2bfe8510cfad28664203fb6515e0
carloni.luca91 <carloni.luca91@gmail.com>
2025-04-11 17:15:38 +0200

    fixing key import

--- devops/import_key.sh
+++ devops/import_key.sh
@@ -5,4 +5,4 @@ function log() {
     echo "$(date +"%Y-%m-%d %H:%m:%S")" "[$1]" "$2"
 }
 
-echo "$GPG_PRIVATE_KEY" | gpg --batch --yes --import && log "INFO" "Successfully imported key"
+echo "$1" | gpg --batch --yes --import && log "INFO" "Successfully imported key"
