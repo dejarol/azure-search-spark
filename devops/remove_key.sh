30c550b98e5b67c132dbd4cc3ecee0a3bbcc208b
carloni.luca91 <carloni.luca91@gmail.com>
2025-05-09 19:27:38 +0200

    fixing key removal

--- devops/remove_key.sh
+++ devops/remove_key.sh
@@ -5,10 +5,11 @@ function log() {
     echo "$(date +"%Y-%m-%d %H:%M:%S")" "[$1]" "$2"
 }
 
-KEY_ID=$(gpg --list-secret-keys --with-colons | awk -F: '/^sec/ {print $5}')
+FPR=$(gpg --list-secret-keys --with-colons | awk -F: '/^fpr:/ {print $10; exit}')
+KEY_ID=$(gpg --list-secret-keys --with-colons | awk -F: '/^sec/ {print $5; exit}')
 if [[ -n $KEY_ID ]];
 then
-    gpg --batch --yes --quiet --delete-secret-keys "$KEY_ID" && log "INFO" "Successfully removed secret key"
+    gpg --batch --yes --quiet --delete-secret-keys "$FPR" && log "INFO" "Successfully removed secret key"
     gpg --batch --yes --quiet --delete-keys "$KEY_ID" && log "INFO" "Successfully removed key"
 else
   log "WARNING" "No key was found"
